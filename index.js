ad3a5dab6d61e25bb8ca493d08520642ae3ca85f
Alexandra Ignatova <alex.ignatova@gmail.com>
2019-08-20 16:44:11 +0200

    -feat: add calendar

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-import { is_weekend } from "./date-helpers.js"
-import { day_name } from "./date-helpers.js"
+import { is_weekend, day_name } from "./date-helpers.js";
 
 const days = document.querySelectorAll(".day");
 const my_grid = document.querySelector("#app-calendar");
 
 for (let i = 1; i <= 31; i++) {
-    let name = day_name(i);
-    let weekend = is_weekend(name);
-    console.log(weekend);
-    my_grid.insertAdjacentHTML("beforeend", `<div class="day ${weekend} ${name}"> ${i}</div>`);
+  let name = day_name(i);
+  let weekend = is_weekend(name);
+  //console.log(name);
+  my_grid.insertAdjacentHTML(
+    "beforeend",
+    `<div class="day ${weekend} ${name}"> ${i}</div>`
+  );
 }
 
-for (let i = 1; i <= 7; i++) {
-    let name = day_name(i);
-    days[i - 1].insertAdjacentHTML("afterbegin", `<p>${name}</p>`);
-}
-
-days.forEach(day => {
-    day.addEventListener('click', event => {
-        event.currentTarget.classList.toggle("selected");
-    });
+document.querySelectorAll(".day").forEach(day => {
+  day.addEventListener("click", event => {
+    event.currentTarget.classList.toggle("selected");
+    console.log("click!!");
+  });
 });
