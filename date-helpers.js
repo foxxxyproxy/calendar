1ad47ad883067eb85e315bf4f54f318e1d049606
Alexandra Ignatova <alex.ignatova@gmail.com>
2019-08-21 12:41:09 +0200

    feat: add getURLParam function

--- date-helpers.js
+++ date-helpers.js
@@ -4,8 +4,8 @@ export const is_weekend = day => {
   } else return "";
 };
 
-export const day_name = day => {
-  const date = createUTCDate(2018, 1, day);
+export const getDayName = (day, month, year) => {
+  const date = createUTCDate(year, month, day);
   const options = { weekday: "short" };
   let name = new Intl.DateTimeFormat("en-US", options).format(date);
   return name;
@@ -13,4 +13,28 @@ export const day_name = day => {
 
 const createUTCDate = (year, month, day) => {
   return new Date(Date.UTC(year, month, day));
+};
+
+export const getURLParam = () => {
+  const parsedURL = new URL(window.location.href);
+  console.log(parsedURL);
+  let searchParam__date = parsedURL.searchParams.get("date");
+
+  if (!searchParam__date) {
+    parsedURL.searchParams.append("date", "27-0-2018");
+    searchParam__date = parsedURL.searchParams.get("date");
+  }
+  console.log(searchParam__date);
+
+  searchParam__date = searchParam__date.split("-");
+  let [day, month, year] = searchParam__date;
+
+  month = toHumanFormat(month);
+  return [month, year];
+};
+
+const toHumanFormat = month => {
+  month = Number.parseInt(month) - 1;
+
+  return month;
 };
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import { is_weekend, day_name } from "./date-helpers.js";
+import { is_weekend, getDayName, getURLParam } from "./date-helpers.js";
 
 const days = document.querySelectorAll(".day");
 const my_grid = document.querySelector("#app-calendar");
+const [month, year] = getURLParam();
 
 for (let i = 1; i <= 31; i++) {
-  let name = day_name(i);
+  let name = getDayName(i, month, year);
   let weekend = is_weekend(name);
-  //console.log(name);
   my_grid.insertAdjacentHTML(
     "beforeend",
     `<div class="day ${weekend} ${name}"> ${i}</div>`
@@ -16,6 +16,6 @@ for (let i = 1; i <= 31; i++) {
 document.querySelectorAll(".day").forEach(day => {
   day.addEventListener("click", event => {
     event.currentTarget.classList.toggle("selected");
-    console.log("click!!");
+    console.log(`it's ${event.currentTarget.classList[1]}`);
   });
 });
